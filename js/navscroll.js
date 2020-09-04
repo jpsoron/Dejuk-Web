4216de71dcb2c3229cccaff977b6851b02e0f537
jpsoron <sorondojp@gmail.com>
2020-09-03 23:33:25 -0300

    various visual, anim and size changes

--- js/navscroll.js
+++ js/navscroll.js
@@ -2,9 +2,16 @@ $(document).ready(function(){
     $(window).scroll(function(){
         if($(this).scrollTop()>0){
             $("#navbar, #dejuklogo").addClass("minimized");
+            $("#override a.navbarlink").removeClass("largelink");
+            $("#navchevron").css("opacity","0");
         }
         else{
             $("#navbar, #dejuklogo").removeClass("minimized");
+            $("#override a.navbarlink").addClass("largelink");
+            $("#navchevron").css("opacity","1");
         }
     })
 })
+$(window).on('beforeunload', function() {
+    $(window).scrollTop(0);
+});
