16f2ca51359a035f6e634cb73615d18caa01104c
jpsoron <sorondojp@gmail.com>
2020-09-05 03:02:34 -0300

    android fixes

--- js/androidcheck.js
+++ js/androidcheck.js
@@ -5,6 +5,7 @@ $(document).ready(function(){
 
     if(Android){
         $("#navbar").addClass("minimized");
+        $("#mainbody").css("visibility","hidden");
         scrollTo("#section1");
     }
 })
--- js/navscroll.js
+++ js/navscroll.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
     $("#mainbody").css("visibility","hidden");
+    var userAgent = navigator.userAgent.toLowerCase(); 
+    var Android = userAgent.indexOf("android") > -1;
+
+    if(Android){
+        scrollTo("#section1");
+        $("#navbar, #dejuklogo").addClass("minimized");
+        $("#mainbody").css("visibility","visible");
+    }else{
     $(window).scroll(function(){
         if($(this).scrollTop()>0){
             $("#mainbody").css("visibility","visible");
@@ -12,7 +20,7 @@ $(document).ready(function(){
             $("#override a.navbarlink").addClass("largelink");
             $("#navchevron").css({"opacity":"1","display":"block"});
         }
-    })
+    })}
 })
 $(window).on('beforeunload', function() {
     $(window).scrollTop(0);
