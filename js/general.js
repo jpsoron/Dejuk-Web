a14d4eb66104c4dd487fa5433f7fbb76c539b5ef
jpsoron <sorondojp@gmail.com>
2020-09-05 15:17:35 -0300

    android fix

--- js/general.js
+++ js/general.js
@@ -1,13 +1,5 @@
 $(document).ready(function(){
     $("#mainbody").css("visibility","hidden");
-    var userAgent = navigator.userAgent.toLowerCase(); 
-    var Android = userAgent.indexOf("android") > -1;
-
-    if(Android){
-        scrollTo("#section1");
-        $("#navbar, #dejuklogo").addClass("minimized");
-        $("#mainbody").css("visibility","visible");
-    }else{
     $(window).scroll(function(){
         if($(this).scrollTop()>0){
             $("#mainbody").css("visibility","visible");
@@ -20,7 +12,7 @@ $(document).ready(function(){
             $("#override a.navbarlink").addClass("largelink");
             $("#navchevron").css({"opacity":"1","display":"block"});
         }
-    })}
+    })
 })
 $(window).on('beforeunload', function() {
     $(window).scrollTop(0);
